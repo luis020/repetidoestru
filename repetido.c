62ee73b2e32a30120480f3145d85b1860a375fbb
luis020 <37189401+luis020@users.noreply.github.com>
2018-10-02 10:18:18 -0600

    Add files via upload

---
+++ repetido.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main()
+{
+
+int=0;
+
+printf("Ingresar el tamaño\n");
+scantf("%¡",&n);
+
+int Matriz[n][n];
+for(int f=0;f<n;f++){
+	for(int c=0;c<n;c++){
+		printf("Ingresar un valor\n");
+		scanf("%¡",&Matriz[f][c]);
+		
+	}
+}
+
+
+
+for(int f=0;f<n;f++){
+	printf("\n");
+	for(int c=0;c<n;c++){
+		printf("%¡", Matriz[f][c]);
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
